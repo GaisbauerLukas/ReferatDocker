8db259bc0895f5817c3434677f8e2a7b63be32b6
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2021-01-06 15:00:55 +0100

    Upload Docker Hub Demo

---
+++ Demo/Container/dockerHubDemo.sh
@@ -0,0 +1,13 @@
+# Image Pull
+docker pull {imageName}
+# Dies ist im Normalfall aber nicht notwendig da beim starten eines Containers das Image, 
+# solang es nicht vorhanden ist, automatisch heruntergeladen wird
+
+# Image Push
+docker push {imageName}
+
+# Login to DockerHub
+docker login
+
+# Ausloggen
+docker logout
