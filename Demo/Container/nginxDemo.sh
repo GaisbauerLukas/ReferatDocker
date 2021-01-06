6f5b9713844112fc86542f734281ef18f4b72625
Lukas Gaisbauer <gaisbauer.lukas@gmx.at>
2021-01-06 15:37:10 +0100

    Extended nginx Demo

--- Demo/Container/nginxDemo.sh
+++ Demo/Container/nginxDemo.sh
@@ -13,3 +13,18 @@ docker stop nginx && docker rm nginx
 #or
 docker rm -f nginx
 #Nginx steht hierbei für den Container Name. Statdessen können auch die ersten beiden Buchstaben der Container id verwendet werden.
+
+# Anlegen eines Volumes
+docker run -p 8080:80 --name nginx -d -v data:/usr/share/nginx/html nginx 
+#Die Daten eines Verzeichnises werden in einen von Docker verwalteten Speicherplatz abgelegt. Dieser wird nach dem Stopen des Containers
+#nicht gelöscht.
+
+# Ansehen von Volumes
+docker volume ls 
+#Listet alle Volumes auf
+docker volume inspect {nameDesVolumes}
+#Gibt dessen Daten wieder. So kann man beispielsweise den Speicherort eines volumes anzeigen
+
+# Anlegen von Bindmounts
+docker run -p 8080:80 --name nginx -d -v /home/lukas/Documents/repos/ReferatDocker/Demo/Container/exampleWebPage:/usr/share/nginx/html nginx
+# Der Pfad am Localen Dateisystem muss immer absolut angegeben werden
